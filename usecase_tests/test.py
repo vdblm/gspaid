b8b46aae16c737799e97deb4bf14f0e868d20d52
= <=>
2018-04-14 12:08:08 +0430

    test improve

--- usecase_tests/test.py
+++ usecase_tests/test.py
@@ -1,15 +1,23 @@
 from selenium import webdriver
-
+import unittest
 import time
 
+
+unit_test = unittest.TestCase
+
+
 browser = webdriver.Chrome('C:\\Users\\vahid\\Desktop\\chromedriver.exe')
+browser.get("file:///D:/Sharif/Term/SAD/gspaid/documents/design/wireframe/general/home.html")
 
-browser.get("https://cw.sharif.edu/login/index.php")
+time.sleep(3)
+name = browser.find_element_by_id("name")
+email = browser.find_element_by_id("email")
+description = browser.find_element_by_id("description")
+name.send_keys("vahid")
+email.send_keys("1")
+description.send_keys("slala")
 
-time.sleep(10)
-username = browser.find_element_by_id("username")
-password = browser.find_element_by_id("password")
-username.send_keys("94105503")
-password.send_keys("1")
-login_attempt = browser.find_element_by_xpath("//*[@type='submit']")
-login_attempt.submit()
+# time.sleep(10)
+submit_attempt = browser.find_element_by_xpath("//*[@type='submit']")
+submit_attempt.submit()
+
