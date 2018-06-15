8961443513d43960fffd0d5102ec1586307bced3
Ali Asgari <altostratous@gmail.com>
2018-06-15 13:49:44 +0430

    Add currency model

--- financial/models.py
+++ financial/models.py
@@ -1,8 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import requests
+import json
 
 
 class Currency(models.Model):
     name = models.CharField(max_length=128, blank=False, null=False)
-    ratio = models.IntegerField(null=False, blank=False)
+
+    @property
+    def ratio(self):
+        conversion_key = 'IRR_' + self.name
+        response = requests.request(
+            'GET',
+            'https://free.currencyconverterapi.com/api/v5/convert?q=%s&compact=ultra' % conversion_key
+        )
+        return float(json.load(response.text)[conversion_key])
