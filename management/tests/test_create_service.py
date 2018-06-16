481c831ece9d173b99b52c212caf5e93e7630a3e
Ali Asgari <altostratous@gmail.com>
2018-06-16 15:35:57 +0430

    Add test send notification

--- management/tests/test_create_service.py
+++ management/tests/test_create_service.py
@@ -59,7 +59,7 @@ class Tests(SeleniumTestCase):
 
     @override_settings(DEBUG=True)
     def test_create_service_new_service_successful(self):
-        self.help_create_service("IRR", 98765, "It is newwwww")
+        self.help_create_service("IRR", 98765, "It is newwwww", "Father's name: ......")
         self.assertTrue('Created request type successfully' in self.web_driver.page_source)
         notification_link = self.web_driver.find_element_by_link_text('Send Notification')
         notification_link.click()
@@ -74,3 +74,12 @@ class Tests(SeleniumTestCase):
             extra_info="You wanna buy it anyway",
         )
         self.assertTrue('duplicate' in self.web_driver.page_source)
+
+    def test_send_notification(self):
+        self.test_create_service_new_service_successful()
+
+        notification_text_element = self.web_driver.find_element_by_id('notification_test')
+        notification_text_element.send_keys('This is a new service we are adding.')
+        notification_text_element.submit()
+
+        self.assertTrue('Notification sent' in self.web_driver.page_source)
