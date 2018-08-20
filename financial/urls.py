2c621f425870e39fc7de3572a66abba54118179a
vdblm <vbalazadehmeresht@ce.sharif.edu>
2018-08-20 23:47:44 +0430

    syntax update

--- financial/urls.py
+++ financial/urls.py
@@ -5,10 +5,9 @@ from financial import views
 app_name = 'financial'
 
 urlpatterns = [
-    url(r'^live_currency/', views.live_currency),
-    url(r'^currency_converter/', views.currency_converter),
-    url(r'^online_wallet/', views.online_wallet, name='online_wallet'),
-    url(r'^charged/', views.charged, name='charged'),
-    url(r'^changed/', views.changed, name='changed'),
-    url(r'^internal_payment/', views.internal_payment, name='internal_payment')
+    url(r'^live_currency/$', views.live_currency),
+    url(r'^currency_converter/$', views.currency_converter),
+    url(r'^online_wallet/$', views.online_wallet, name='online_wallet'),
+    url(r'^charged/$', views.charged, name='charged'),
+    url(r'^internal_payment/$', views.internal_payment, name='internal_payment')
 ]
