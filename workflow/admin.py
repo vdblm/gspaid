a5dac086b71f3664dfe756e42a0225106e153a87
Ali Asgari <altostratous@gmail.com>
2018-08-21 08:10:43 +0430

    Fix to get the site working

--- workflow/admin.py
+++ workflow/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from solo.admin import SingletonModelAdmin
-from .models import ExchangeRequest
-
-admin.site.register(ExchangeRequest, SingletonModelAdmin)
