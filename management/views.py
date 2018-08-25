0f3fbfce5c3f932fc472ebf6e65997daf05a2133
vdblm <vbalazadehmeresht@ce.sharif.edu>
2018-08-25 20:54:33 +0430

    Add view function for NewRequestType

--- management/views.py
+++ management/views.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import user_passes_test
 from django.shortcuts import render
 from django.contrib import messages
 
-from management.forms import ContactAdminForm
+from management.forms import ContactAdminForm, NewRequestTypeForm
 
 
 def contact_admin(request):
@@ -26,8 +27,15 @@ def management_dashboard(request):
     return render(request, "management/management_dashboard.html")
 
 
+@user_passes_test(lambda user: user.is_superuser)
 def add_request_type(request):
-    return render(request, 'management/new_request_type.html')
+    new_request_type_form = NewRequestTypeForm(request.POST if request.method == 'POST' else None)
+    if request.method == 'POST':
+        if new_request_type_form.is_valid():
+            new_request_type_form.save()
+            messages.add_message(request, messages.INFO, 'New Request Type Added')
+
+    return render(request, 'management/new_request_type.html', {'new_request_type_from': new_request_type_form})
 
 
 def settings(request):
