7eea89e6a3e5a434728d9fd35ca54687d7213482
vdblm <vbalazadehmeresht@ce.sharif.edu>
2018-08-25 20:53:06 +0430

    Add form for NewRequestType

--- management/forms.py
+++ management/forms.py
@@ -1,9 +1,19 @@
 from django.forms import ModelForm
-
 from management.models import Ticket
+from workflow.models import RequestType
 
 
 class ContactAdminForm(ModelForm):
     class Meta:
         model = Ticket
         exclude = []
+
+
+class NewRequestTypeForm(ModelForm):
+    class Meta:
+        model = RequestType
+        fields = ['name', 'description', 'wage_rule', 'currency', 'amount', 'information']
+
+    def is_valid(self):
+        self.instance.user = self.user
+        return super().is_valid()
